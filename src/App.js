5f79d1ab9157eb8d484b991b5c722f1dee9c689d
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-29 21:40:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../src/App.css";
+import Signin from "./Components/Signin";
+import { Routes, Route } from "react-router-dom";
+import Profile from "./Components/Profile";
+import store from "./reduxStore/store";
+import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ return(
+  <Provider store={store}>
+  <div>
+    
+    <Routes>
+      <Route path="" element={<Signin/>}  />
+       <Route path="/profile" element={<Profile/>}  /> 
+      
+    </Routes>
+    
+  </div>
+  </Provider>
+ )
+};
 
 export default App;
